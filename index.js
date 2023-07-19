837c5303280e4b09bc9117d6270851820a64f402
yadubabu <100501154+yadubabu@users.noreply.github.com>
2023-07-19 11:59:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,57 +21,11 @@ app.get("/", async (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/addtrans", async (req, res) => {
-  const { name, type, amount, date } = req.body;
-  try {
-    const newTrans = new Transactions({
-      name,
-      type,
-      amount,
-      date,
-    });
-    await newTrans.save();
-    console.log(newTrans);
-
-    return res.json(await newTrans.type);
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.get("/gettrans", async (req, res) => {
-  try {
-    return res.json(await Transactions.find());
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.post("/adduser", async (req, res) => {
-  const { name, email, password, confirmpassword, pancard, phone } = req.body;
-
-  try {
-    const newUser = new BudgetUser({
-      name,
-      email,
-      password,
-      confirmpassword,
-      pancard,
-      phone,
-    });
-    await newUser.save();
-    return res.status(200).json("User Registered Successfully");
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.post("/loginuser", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const getUser = await BudgetUser.findOne({ email });
-    return await res.status(200).json(getUser);
-  } catch (err) {
-    console.log(err);
-  }
-});
+app.use("/addtrans", require("./routes/transRouter"));
+app.use("/gettrans", require("./routes/transRouter"));
+app.use("/gettotals", require("./routes/transRouter"));
+app.use("/", require("./routes/userRouter"));
+// app.use("/", require("./routes/userRouter"));
 
 app.listen(5000, () => {
   console.log("Server Running on Port 5000");
